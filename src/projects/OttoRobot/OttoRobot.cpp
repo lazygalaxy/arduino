02b1cea825fd4df033be72deb23f67d8d56a5362
Evangelos Papakonstantis <vangos@lazygalaxy.com>
2021-04-27 21:45:52 +0200

    use dependency management

--- src/projects/OttoRobot/OttoRobot.cpp
+++ src/projects/OttoRobot/OttoRobot.cpp
@@ -1,55 +1,27 @@
-#include <Arduino.h>
-#include <Servo.h>
+#include <Otto9.h>
+Otto9 Otto;
 
-Servo leftLeg;
-Servo rightLeg;
-Servo leftFoot;
-Servo rightFoot;
-
-int pos;
-
-void stand() {
-  leftLeg.write(90);
-  rightLeg.write(90);
-  leftFoot.write(90);
-  rightFoot.write(90);
-
-  delay(1000);
-}
-
-void jump() {
-  leftFoot.write(20);
-  rightFoot.write(160);
-
-  delay(1000);
-}
+#define PIN_YL 2       // left leg, servo[0]
+#define PIN_YR 3       // right leg, servo[1]
+#define PIN_RL 4       // left foot, servo[2]
+#define PIN_RR 5       // right foot, servo[3]
+#define PIN_Trigger 8  // ultrasound
+#define PIN_Echo 9     // ultrasound
+#define PIN_Buzzer 13  // buzzer
 
 void setup() {
-  leftLeg.attach(2);
-  rightLeg.attach(3);
-  leftFoot.attach(4);
-  rightFoot.attach(5);
-
-  stand();
+  Otto.init(PIN_YL, PIN_YR, PIN_RL, PIN_RR, true, A6, PIN_Buzzer, PIN_Trigger,
+            PIN_Echo);
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  // leftFoot.write(0);
-  // delay(2000);
-  // leftFoot.write(180);
-  // delay(2000);
-
-  jump();
-  stand();
-  // for (pos = 90; pos <= 150; pos += 1) {
-  //   rightLeg.write(pos);
-
-  //   delay(15);
-  // }
-  // for (pos = 150; pos >= 90; pos -= 1) {
-  //   rightLeg.write(pos);
-  //   rightFoot.write(pos);
-  //   delay(15);
-  // }
+  Otto.sing(S_OhOoh2);
+  Otto.sing(S_OhOoh2);
+  Otto.walk(1, 1000, 1);  // FORWARD
+  Otto.walk(1, 1000, 1);  // FORWARD
+  Otto.walk(1, 1000, 1);  // FORWARD
+  delay(2 * 1000);
+  Otto.turn(1, 1000, -1);  // RIGHT
+  Otto.turn(1, 1000, -1);  // RIGHT
+  delay(2 * 1000);
 }
