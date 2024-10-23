d9ae26874bcd17f52ff4a060c54ce2c9f39c30ca
LazyGalaxy <vangos@lazygalaxy.com>
2024-10-23 23:05:26 +0200

    working example

--- lib/LazyGalaxyAudioPlayer/LazyGalaxyAudioPlayer.cpp
+++ lib/LazyGalaxyAudioPlayer/LazyGalaxyAudioPlayer.cpp
@@ -6,42 +6,51 @@
 
 #include <LazyGalaxyAudioPlayer.h>
 
-MyAudioPlayer::MyAudioPlayer(char volume) : PinComponent(255)
+MyAudioPlayer::MyAudioPlayer(uint8_t volume) : PinComponent(255)
 {
   _volume = volume;
 }
 
 void MyAudioPlayer::setup()
 {
-  _softwareSerial = new SoftwareSerial(RX_PIN, TX_PIN);
-  _softwareSerial->begin(9600);
+  SoftwareSerial secondarySerial(10, 11); // RX, TX
+  _player = new DfMp3(secondarySerial);
 
-  DEBUG_DEBUG("Initializing AudioPlayer ...");
-  if (_player.begin(*_softwareSerial, true, false))
-  {
-    DEBUG_INFO("AudioPlayer OK");
-    _player.volume(_volume);
-  }
-  else
-    DEBUG_ERROR("Audio Player failed");
+  _player->begin();
+
+  uint16_t version = _player->getSoftwareVersion();
+  Serial.print("version ");
+  Serial.println(version);
+
+  _player->setVolume(_volume);
+  uint16_t volume = _player->getVolume();
+  Serial.print("volume ");
+  Serial.println(volume);
+
+  uint16_t count = _player->getTotalTrackCount(DfMp3_PlaySource_Sd);
+  Serial.print("files ");
+  Serial.println(count);
+
+  Serial.println("starting...");
 }
 
 void MyAudioPlayer::reset()
 {
   DEBUG_DEBUG("reset audio player");
   Component::reset();
+  _player->reset();
 }
 
-void MyAudioPlayer::play(int index)
+void MyAudioPlayer::play(uint16_t index)
 {
   DEBUG_DEBUG("play audio 1");
-  _player.play(index);
+  _player->playMp3FolderTrack(index);
   DEBUG_DEBUG("play audio 2");
 }
 
 void MyAudioPlayer::stop()
 {
-  _player.stop();
+  _player->stop();
 }
 
 unsigned long MyAudioPlayer::update(unsigned long time)
--- lib/LazyGalaxyAudioPlayer/LazyGalaxyAudioPlayer.h
+++ lib/LazyGalaxyAudioPlayer/LazyGalaxyAudioPlayer.h
@@ -8,27 +8,83 @@
 #define LAZYGALAXYAUDIOPLAYER_H
 
 #include <LazyGalaxyCommon.h>
+#include <DFMiniMp3.h>
 #include <SoftwareSerial.h>
-#include <DFRobotDFPlayerMini.h>
 
 #define RX_PIN 10
 #define TX_PIN 11
 
+class Mp3Notify;
+typedef DFMiniMp3<SoftwareSerial, Mp3Notify> DfMp3;
+
+class Mp3Notify
+{
+public:
+  static void PrintlnSourceAction(DfMp3_PlaySources source, const char *action)
+  {
+    if (source & DfMp3_PlaySources_Sd)
+    {
+      Serial.print("SD Card, ");
+    }
+    if (source & DfMp3_PlaySources_Usb)
+    {
+      Serial.print("USB Disk, ");
+    }
+    if (source & DfMp3_PlaySources_Flash)
+    {
+      Serial.print("Flash, ");
+    }
+    Serial.println(action);
+  }
+  static void OnError([[maybe_unused]] DfMp3 &mp3, uint16_t errorCode)
+  {
+    // see DfMp3_Error for code meaning
+    Serial.println();
+    Serial.print("Com Error ");
+    Serial.println(errorCode);
+  }
+  static void OnPlayFinished([[maybe_unused]] DfMp3 &mp3, [[maybe_unused]] DfMp3_PlaySources source, uint16_t track)
+  {
+    Serial.print("Play finished for #");
+    Serial.println(track);
+
+    // start next track
+    // track += 1;
+    // this example will just start back over with 1 after track 3
+    // if (track > 3)
+    //{
+    //    track = 1;
+    //}
+    // dfmp3.playMp3FolderTrack(track); // sd:/mp3/0001.mp3, sd:/mp3/0002.mp3, sd:/mp3/0003.mp3
+  }
+  static void OnPlaySourceOnline([[maybe_unused]] DfMp3 &mp3, DfMp3_PlaySources source)
+  {
+    PrintlnSourceAction(source, "online");
+  }
+  static void OnPlaySourceInserted([[maybe_unused]] DfMp3 &mp3, DfMp3_PlaySources source)
+  {
+    PrintlnSourceAction(source, "inserted");
+  }
+  static void OnPlaySourceRemoved([[maybe_unused]] DfMp3 &mp3, DfMp3_PlaySources source)
+  {
+    PrintlnSourceAction(source, "removed");
+  }
+};
+
 class MyAudioPlayer : public PinComponent
 {
 public:
-  MyAudioPlayer(char volume = 15);
+  MyAudioPlayer(uint8_t volume = 15);
   void setup() override;
   void reset() override;
   unsigned long update(unsigned long time) override;
 
-  void play(int index);
+  void play(uint16_t index);
   void stop();
 
 private:
-  char _volume;
-  DFRobotDFPlayerMini _player;
-  SoftwareSerial *_softwareSerial;
+  uint8_t _volume;
+  DfMp3 *_player;
 };
 
 #endif
