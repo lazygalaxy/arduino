2222a943f0da0aadb6e9c2933f6e9242e8884110
LazyGalaxy <vangos@lazygalaxy.com>
2024-10-24 08:11:57 +0200

    example not working

--- src/examples/sound/AudioPlayerExample/AudioPlayerExample.cpp
+++ src/examples/sound/AudioPlayerExample/AudioPlayerExample.cpp
@@ -1,32 +1,30 @@
 /*
-   PlayWAVExample.cpp - Example on playing a WAV file.
+   AudioPlayerExample.cpp - Example on playing a MP3 or WAV files.
    Created by LazyGalaxy - Evangelos Papakonstantis, September 22, 2024.
    Released into the public domain.
 */
 
 #include <LazyGalaxySystem.h>
 #include <LazyGalaxyTimer.h>
-#include <LazyGalaxySDCard.h>
-#include <LazyGalaxySpeaker.h>
+#include <LazyGalaxyAudioPlayer.h>
 
-MySpeaker *speaker = new MySpeaker(D9);
+MyAudioPlayer *audioPLayer = new MyAudioPlayer();
 
 void playOffWav(unsigned long time)
 {
-    speaker->playWav("OFF.wav");
+    audioPLayer->play(1);
 }
 
 void setup()
 {
-    Serial.begin(9600);
+    Serial.begin(115200);
     Debug.setDebugLevel(DBG_VERBOSE);
 
-    System::add(new MySDCard(D10));
-    System::add(speaker);
+    System::add(audioPLayer);
 
     System::setup();
 
-    speaker->playWav("ON.wav");
+    audioPLayer->play(1);
     // schedule a task/function callback to play in the future
     Timer::schedule(500, playOffWav);
 }
