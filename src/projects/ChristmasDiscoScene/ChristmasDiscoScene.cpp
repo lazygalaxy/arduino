f563c5aead9746e3de1ec3e65176be28038c5e35
LazyGalaxy <vangos@lazygalaxy.com>
2025-12-14 00:33:54 +0100

    remove disco plays only christmas songs.

--- src/projects/ChristmasDiscoScene/ChristmasDiscoScene.cpp
+++ src/projects/ChristmasDiscoScene/ChristmasDiscoScene.cpp
@@ -4,76 +4,58 @@
    Released into the public domain.
 */
 
+#include <LazyGalaxyButton.h>
 #include <LazyGalaxyCircuit.h>
-#include <LazyGalaxyTimer.h>
 #include <LazyGalaxyLED.h>
 #include <LazyGalaxyAudioPlayer.h>
 
 // the speaker volume from 0 to 30
-static const uint8_t SPEAKER_VOLUME = 25;
-// how long each scene lasts in milliseconds
-static const unsigned int SCENE_DURATION_MILLIS = 30000;
-// how long does the disco light blink
-static const unsigned int DISCO_BLINK_MILLIS = 400;
+static const uint8_t SPEAKER_VOLUME = 20;
 
-// keep track of the Christmas vs Disco Scenes
-bool isChristmasScene = true;
-// keep track of teh tracks so we can cycle through them
-uint8_t totalChristmasTracks;
-uint8_t totalDiscoTracks;
-uint8_t christmasTrackIndex = 0;
-uint8_t discoTrackIndex = 0;
-
+bool flag = false;
 // all scene components
 MyLED chrismasLED(D2);
-MyLED discoLED(D3);
+// MyLED discoLED(D3);
+MyButton button(D5);
+
 MyAudioPlayer audioPlayer(D10, D11, SPEAKER_VOLUME);
 
-unsigned long playScene(unsigned long time)
+void buttonPress(unsigned long time, uint8_t clicks)
 {
-  if (isChristmasScene)
+  // Serial.print("press");
+  if (clicks > 0)
   {
-    // our christmas scene logic
-    discoLED.stopBlink();
-    discoLED.setLight(false);
-    chrismasLED.setLight(true);
-    christmasTrackIndex = ++christmasTrackIndex > totalChristmasTracks ? 1 : christmasTrackIndex;
-    audioPlayer.play(1, christmasTrackIndex);
+    // Serial.print("click");
+    if (!flag)
+    {
+      audioPlayer.playRandom(1);
+      chrismasLED.setLight(true);
+      // Serial.print("play");
+    }
+    else
+    {
+      audioPlayer.stop();
+      chrismasLED.setLight(false);
+      // Serial.print("stop");
+    }
+    flag = !flag;
   }
-  else
-  {
-    // our disco scene logic
-    chrismasLED.setLight(false);
-    discoLED.startBlink(DISCO_BLINK_MILLIS);
-    discoTrackIndex = ++discoTrackIndex > totalDiscoTracks ? 1 : discoTrackIndex;
-    audioPlayer.play(2, discoTrackIndex);
-  }
-
-  // switch the scene flag
-  isChristmasScene = !isChristmasScene;
-  // schedule the timer for the next scene
-  return time + SCENE_DURATION_MILLIS;
 }
 
 void setup()
 {
-  // Circuit setup
-  Circuit::add(&chrismasLED);
-  Circuit::add(&discoLED);
+  // Serial.begin(115200);
+  randomSeed(analogRead(A0));
+
   Circuit::add(&audioPlayer);
+  Circuit::add(&button);
   Circuit::setup();
 
-  // get the total number of tracks now that we have setup the audio player
-  totalChristmasTracks = audioPlayer.getNumTracks(1);
-  totalDiscoTracks = audioPlayer.getNumTracks(2);
-
-  // schedule the timer immediatly to play teh first scene
-  Timer::schedule(0, playScene);
+  button.startClicksCallback(buttonPress);
 }
 
 void loop()
 {
   // loop through the circuit components and the timers
   Circuit::loop();
-  Timer::loop();
 }
